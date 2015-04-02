cda78abdf6b67321d0edd676654c8fbf511b0e32
Jacob Quant <jacobq@gmail.com>
2015-04-02 00:56:58 +0000

    WIP: heatmap deps & video output folder

--- server/heatmap/dep.sh
+++ server/heatmap/dep.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 # Uses easy_install to install required modules for the heatmap/analytics code to work 
+
+# OS-level deps may also be needed:
+# sudo apt-get install libfreetype2-dev libxft-dev
+# sudo apt-get install libpng-dev
+# sudo apt-get install libblas-common libopenblas-base libopenblas-dev
+# sudo apt-get install gfortran
+
+sudo easy_install matplotlib
+sudo easy_install numpy
+sudo easy_install scipy
 sudo easy_install scitools
+
