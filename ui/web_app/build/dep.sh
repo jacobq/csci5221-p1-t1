cc4dc3dc87e8a94554da2c0000323dcc1c45b683
Jacob Quant <jacobq@gmail.com>
2015-04-20 23:13:30 -0500

    Added moment to dep.sh for web_app

--- ui/web_app/build/dep.sh
+++ ui/web_app/build/dep.sh
@@ -1,1 +1,1 @@
-sudo npm install debug react reflux gulp gulp-util gulp-livereload vinyl-source-stream  browserify watchify reactify gulp-uglify jquery-browserify underscore chart.js react-chartjs tween.js
+sudo npm install debug react reflux gulp gulp-util gulp-livereload vinyl-source-stream  browserify watchify reactify gulp-uglify jquery-browserify underscore chart.js react-chartjs tween.js moment
