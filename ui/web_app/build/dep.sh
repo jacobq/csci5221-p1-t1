22a2475e3f1376d284127ebcec47bac051f3c922
Jacob Quant <jacobq@gmail.com>
2015-04-20 23:05:01 -0500

    Fixed dep.sh merge mistake

--- ui/web_app/build/dep.sh
+++ ui/web_app/build/dep.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-sudo npm install debug react reflux gulp gulp-util gulp-livereload vinyl-source-stream  browserify watchify reactify gulp-uglify tweenjs
-=======
 sudo npm install debug react reflux gulp gulp-util gulp-livereload vinyl-source-stream  browserify watchify reactify gulp-uglify jquery-browserify underscore chart.js react-chartjs tween.js
->>>>>>> 6b5e69e4289c838f53111957a8bd72d9e4733662
